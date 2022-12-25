4ef241bd288d5bc8cf59e1493382675723f6e7a1
Mehmet Erden <merden2512@gmail.com>
2022-12-25 09:06:36 +0300

    v1

--- src/Ders14/C03_sifreKontrolu.java
+++ src/Ders14/C03_sifreKontrolu.java
@@ -1,8 +1,12 @@
 package Ders14;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class C03_sifreKontrolu {
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         //Soru 2- Kullanicidan bir sifre girmesini isteyin. Girilen sifreyi asagidaki sartlara gore
         //kontrol edin ve sifredeki hatalari yazdirin.
@@ -12,44 +16,67 @@ public class C03_sifreKontrolu {
         //- Sifre buyuk harf icermelidir
         //- Sifre ozel karakter icermelidir
         //- Sifre en az 8 karakter olmalidir
-      /*  Scanner str = new Scanner(System.in);
-        System.out.println("lutfen bir sifrenizi girin:");
 
-        boolean sifreKontrol = true;
-        int flag = 0;
-        do { //- Sifre kucuk harf icermelidir
-            String sifre = str.nextLine();
-            for (int i = 0; i < sifre.length() - 1; i++) {
-                if (!(sifre.charAt(i) >= 'a' || sifre.charAt(i) <= 'z')) {
-                    System.out.println(" Sifre kucuk harf icermelidir");
-                    flag++;
-                }
-                //- Sifre buyu k harf icermelidir
-                if (!(sifre.charAt(i) >= 'A' || sifre.charAt(i) <= 'Z')) {
-                    System.out.println(" Sifre kucuk harf icermelidir");
-                    flag++;
+                    /*Regex (Regular Expressions)
+            \\s : space \\S : space olmayan hersey
+            \\s+ : yanyana birden fazla space
+            \\d : digits \\D : digit olmayan hersey
+            \\w : harf veya rakam \\W : harf veya rakam olmayan hersey
+
+         */i) >= 'a' && sifre.charAt(i) <= 'z')) {
+                    kucukHarfSayisi++;
                 }
             }
-            //- Sifre ozel karakter icermelidir
-            if (!sifre.contains("\\W")) {
-                System.out.println("sifre ozel karakter icermelidir");
-                flag++;
-            }
-            if (sifre.length() < 8) {
-                System.out.println("Sifre en az 8 karakter olmalidir");
-                flag++;
-            }
-            if (flag == 0) {
-                sifreKontrol = false;
-            }
-            while (sifreKontrol) {
+            if (kucukHarfSayisi == 0) {
+                System.out.println("sifre kucuk harf icermelidir");
+                hataliSifre++;}
+            int buyukHarfSayisi = 0;
+            for (int i = 0; i < sifre.length(); i++) {
+                if (!(sifre.charAt
+                String str="oxkvcjnjn151&";
+        System.out.println(str.contains("&"));
 
-            }
-            System.out.println("sifre kaydi basarili");
+        //sifreDuzelelt();
 
-        }}
     }
 
-       */
+    /*private static void sifreDuzelelt() {
 
-    }}
+
+        boolean sifremDogruMu = false;
+        int hataliSifre = 0;
+
+        do {
+            System.out.println("Lutfen sifre girinizi giriniz: ");
+            String sifre = scan.nextLine();
+
+            if (sifre.length() < 8) {
+                System.out.println("sifre uzunlugu 8 ve daha fazla karakter icermelidir");
+                hataliSifre++;
+            }
+
+            if (!sifre.contains("\\W")) {
+                System.out.println("sifre ozel karakter icermelidir");
+                hataliSifre++;
+            }
+
+
+            int kucukHarfSayisi = 0;
+            for (int i = 0; i < sifre.length(); i++) {
+                if ((sifre.charAt((i) >= 'A' && sifre.charAt(i) <= 'Z')) {
+                    buyukHarfSayisi++;
+                }
+            }
+            if (buyukHarfSayisi == 0) {
+                System.out.println("sifre buyuk harf icermelidir");
+                hataliSifre++;}
+
+            if (hataliSifre == 0) {
+                sifremDogruMu = true;
+                System.out.println("Sifre basari ile kaydedildi ");}
+        } while (sifremDogruMu);
+
+    }
+
+     */
+}
---
+++ src/Ders14/C03_sifrebranch.java
@@ -0,0 +1,7 @@
+package Ders14;
+
+public class C03_sifrebranch {
+    public static void main(String[] args) {
+
+    }
+}
