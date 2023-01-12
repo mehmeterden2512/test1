fc7066be11afe38d862c2c0020cbe12c7c0a2fd8
Mehmet Erden <merden2512@gmail.com>
2023-01-12 20:23:50 +0300

    mentoring

--- src/Ders01/soru2LinkedList.java
+++ src/Ders01/soru2LinkedList.java
@@ -1,8 +1,6 @@
 package Ders01;
 
-import java.util.InputMismatchException;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class soru2LinkedList {
     /*
@@ -13,27 +11,64 @@ public class soru2LinkedList {
     ogrenci sayisi : …..
     ortalama altindaki ogrenci sayisi : ….
     ortalamanin 10 puan alt ve ustunde olan ogrenci sayisi :
+
          */
+    static double ogrenciNotu;
+    static String ogrenciAdiSoyadi;
+
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        double notLarToplami=0;
-        double notOrtalama=0;
-        int ogrenciSayisi=0;
-        double ogrenciNotu;
-        LinkedList<Integer> ogrenciNotlari=new LinkedList<>();
-        boolean devam=true;
-        while (devam){
-            ogrenciNotu=scan.nextDouble();
-            ogrenciSayisi++;
+        Scanner scan = new Scanner(System.in);
+        double notLarToplami = 0;
+        double notOrtalama = 0;
+        int ogrenciSayisi = 0;
+
+
+        Map<Integer, Double> ogrenciNotlari = new HashMap<>();
+
+        boolean devam = true;
+        while (devam) {
+
+
             try {
-                System.out.println("bitirmek icin q ya basın ogrenci notu: ");
-                notLarToplami+=scan.nextInt();
+
+                System.out.println("cıkıs icin Q giriniz \nogrenci notu: ");
+                ogrenciNotu = scan.nextDouble();
+                ogrenciSayisi++;
+                notLarToplami += ogrenciNotu;
+                scan.nextLine();
+                ogrenciNotlari.put(ogrenciSayisi, ogrenciNotu);
 
             } catch (InputMismatchException e) {
-                if (scan.nextInt()=='q'||scan.nextInt()=='Q'){
-                    System.out.println("ogrenci not ortalamasi : "+(notLarToplami/ogrenciSayisi));
+                System.out.println("not girmeyi bitirmek istiyorsaniz Q ya basın devam etmek icin herhangi bir tusa basın");
+                scan.nextLine();
+                Character tercih = scan.next().charAt(0);
+
+                if (tercih == 'Q' || tercih == 'q') {
+                    System.out.println("ogrenci sayısı : " + ogrenciSayisi + "\nogrenci not ortalamasi : " + (notLarToplami / ogrenciSayisi));
+                    devam = false;
                 }
             }
         }
+        int ortAltOgrSayisi = 0;
+        int ort10PuanUstOgrSayisi = 0;
+        int ort10PuanAltOgrSayisi = 0;
+
+        for (Double each : ogrenciNotlari.values()
+        ) {
+            if (each < (notLarToplami / ogrenciSayisi)) {
+                ortAltOgrSayisi++;
+            }
+            if (each < ((notLarToplami / ogrenciSayisi) - 10)) {
+                ort10PuanAltOgrSayisi++;
+            }
+            if (each < (notLarToplami / ogrenciSayisi + 10)) {
+                ort10PuanUstOgrSayisi++;
+            }
+        }
+        System.out.println("ortalama altında ki ogrenci sayisi= " + ortAltOgrSayisi +
+                "\nortalamanın 10 puan altındaki ogrenci sayisi= " + ort10PuanAltOgrSayisi +
+                "\nortalama 10 puan ustundeki ogrenci sayisi= " + ort10PuanUstOgrSayisi);
+        System.out.println(ogrenciNotlari);//Gözle kontrol icin Eklenmiştir
+
     }
 }
---
+++ src/a_01interview/MethodDepom.java
@@ -0,0 +1,124 @@
+package a_01interview;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class MethodDepom {
+   static Scanner scan=new Scanner(System.in);
+    public static Map<Integer, String> mapOlustur() {
+        List<String> team3 = new ArrayList<>(Arrays.asList("Yücel Bey", "Mehmet Dag", "Vahit Bey", "Duygu Günaydin", "Erman Bey", "F.Betül Alptekin",
+                "Hamza Cicek", "Hüseyin Emre", "Mehmet Meral", "Tugce Bol", "Yahya Bey", "Zehranur Avcibas", "Mehmet Erden", "Hakan Bey", "Muhammet Bey"));
+        Map<Integer,String> fellowShipTeam=new HashMap<>();
+        int key=1;
+        for (int i = 0; i <team3.size() ; i++) {
+            fellowShipTeam.put(key,team3.get(i));
+            key++;
+        }
+        return fellowShipTeam;
+    }/*
+    public static String soruGonder(Map<Integer,String> fellowShipTeam){
+        Set<Map.Entry<Integer, String>> entrySet = fellowShipTeam.entrySet();//Map deki kkey ve valueleri getirdi.
+        Random rnd=new Random(15);
+
+        try {
+            FileInputStream fis=new FileInputStream("src/a_01interview/ocak13Sorular");//dosya yolu ekle
+            int k=0;
+            while ((k=fis.read())!=(-1)){
+                if (k=='?'){
+                   String soru= fellowShipTeam.get(rnd)+(char)k;
+                    System.out.print((char)k);
+                   return soru;
+
+                }else {
+                    System.out.print((char)k);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("dosya bulunamadı");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+     return "bir sonraki soru icin soruGonder methodu cagirin";
+    }
+    */
+    public static Map<Integer, String> sorularMap() {
+        List<Integer> soruNo=new ArrayList<>(Arrays.asList(31,32,33,34,35,36,37,38,39,40,41,42,43,44,45));
+        List<String> sorularList=new ArrayList<>(Arrays.asList(" String’ler ve StringBuilder’lar arasındaki farklar?"
+                ," Constructor’lar inherited edilebilir mi?"
+                ," ClassLoader nedir?"
+                ," Kaç tane ClassLoader vardır ve nelerdir?"
+                ," Java’da String’ler neden doğası gereği değişmezdir?"
+                ," Array ve ArrayList arasındaki farklar nelerdir?"
+                ," Java da Map nedir?"
+                ," Java da Collection Class nedir?"
+                ," Collection Class’lar neler içerirler açıklayınız?"
+                ," Polymorphism nedir?"
+                ," Kaç çeşit Polymorphism vardır?"
+                ," Java’da Abstraction (soyutlama) nedir?"
+                ," Java da Interface ne anlam ifade eder?"
+                ," Abstract Class ile Interface arasındaki farklar nelerdir (Bu soru olmazsa olmazlardan)?"
+                ," Java da Inheritance (kalıtım-miras) nedir?"));
+        Map<Integer,String> sorularMap=new HashMap<>();
+        for (int i = 0; i <soruNo.size() ; i++) {
+            sorularMap.put(soruNo.get(i),sorularList.get(i));
+
+        }
+        return sorularMap;
+    }
+
+    public static Map<String, String> soruYolla() {
+        List<String> team3 = new ArrayList<>(Arrays.asList("Yücel Bey", "Mehmet Dag Bey", "Vahit Bey", "Duygu Günaydin Hanim", "Erman Bey", "F.Betül Alptekin Hanim",
+                "Hamza Cicek Bey", "Hüseyin Emre Bey", "Mehmet Meral Bey", "Tugce Bol Bey", "Yahya Bey", "Zehranur Avcibas Hanim", "Mehmet Erden", "Hakan Bey", "Muhammet Bey"));
+        List<String> sorularList=new ArrayList<>(Arrays.asList(" String’ler ve StringBuilder’lar arasındaki farklar?"
+                ," Constructor’lar inherited edilebilir mi?"
+                ," ClassLoader nedir?"
+                ," Kaç tane ClassLoader vardır ve nelerdir?"
+                ," Java’da String’ler neden doğası gereği değişmezdir?"
+                ," Array ve ArrayList arasındaki farklar nelerdir?"
+                ," Java da Map nedir?"
+                ," Java da Collection Class nedir?"
+                ," Collection Class’lar neler içerirler açıklayınız?"
+                ," Polymorphism nedir?"
+                ," Kaç çeşit Polymorphism vardır?"
+                ," Java’da Abstraction (soyutlama) nedir?"
+                ," Java da Interface ne anlam ifade eder?"
+                ," Abstract Class ile Interface arasındaki farklar nelerdir (Bu soru olmazsa olmazlardan)?"
+                ," Java da Inheritance (kalıtım-miras) nedir?"));
+        Map<String,String> eslestirme=new HashMap<>();
+        for (int i = 0; i <team3.size() ; i++) {
+
+                    eslestirme.put(team3.get(i),sorularList.get(i));
+        }
+            return eslestirme;
+    }
+    public static void benimSorum(){
+        System.out.println("*****Fellowship Team **** Hepiniz Hoş Geldiniz.\nBaslamak icin ve soru gecişleri icin enter kullanın");
+
+        String kataKulli=scan.nextLine();
+
+        //Map<Integer, String> fellowShipTeam = MethodDepom.mapOlustur();
+        //Map<Integer,String> sorularYeniMap=MethodDepom.sorularMap();
+        //Set<Map.Entry<Integer, String>> entrySetSorular = sorularYeniMap.entrySet();
+
+        //System.out.println(MethodDepom.soruYolla(fellowShipTeam, sorularYeniMap));
+        Map<String,String> sorular=MethodDepom.soruYolla();
+        Set<Map.Entry<String, String>> entries = sorular.entrySet();
+
+
+        Random rnd=new Random();
+        int tutulanSayi;
+
+        for (Map.Entry<String, String> stringStringEntry : entries)
+        {
+            String team3="fellowShip";
+            int index=0;
+            String key=team3.substring(index,index+1);
+            ++index;
+            System.out.println(stringStringEntry.getKey()+" =====>>> "+ stringStringEntry.getValue());
+            String str=scan.nextLine();
+        }
+
+    }
+}
